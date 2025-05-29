ec11c108e3706d7a4383bb05b81c535e425b64bf
Rasika <rasikamohite731@gmail.com>
2025-05-29 17:18:09 +0530

    Recreate Common component

--- frontend/src/components/common/Loader.tsx
+++ frontend/src/components/common/Loader.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Loader = () => {
   return (
     <div className="flex justify-center items-center py-6">
@@ -8,7 +6,10 @@ const Loader = () => {
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
+        role="status"
+        aria-label="Loading"
       >
+        <title>Loading</title>
         <circle
           className="opacity-25"
           cx="12"
@@ -16,12 +17,12 @@ const Loader = () => {
           r="10"
           stroke="currentColor"
           strokeWidth="4"
-        ></circle>
+        />
         <path
           className="opacity-75"
           fill="currentColor"
           d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-        ></path>
+        />
       </svg>
     </div>
   );
